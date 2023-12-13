d8210493546e2edc6b8d14984a104c0b0180bbda
Hitesh Saini <hitesh9694saini@gmail.com>
2023-12-13 11:27:49 +0530

    add

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 app.use('/ping', function (req, res) {
