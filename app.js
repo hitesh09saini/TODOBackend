68626864d87633cafee2c816c6fc016639d5bf3a
Hitesh Saini <hitesh9694saini@gmail.com>
2023-12-13 11:05:52 +0530

    j

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ const cors = require('cors');
 
 const app = express();
 
+const corsOptions = {
+  origin: [process.env.CLIENT_URL],
+  credentials: true, // Use 'credentials' instead of 'Credentials'
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Explicitly specify allowed methods
+};
+
 // config
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
-app.use(cors());
+app.use(cors(corsOptions));
 
 
 app.use('/ping', function (req, res) {
